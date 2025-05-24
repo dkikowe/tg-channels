bf21b4fbb68c335e35fff858a95e6341b89ecc43
dkikowe <dkikowe@gmail.com>
2025-05-24 22:20:41 +0500

    sdsds

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "tg-channels-back-production.up.railway.app",
+  baseURL: "https://tg-channels-back-production.up.railway.app",
   //   baseURL: "tg-channels-back-production.up.railway.app", // или https://api.site.com
   // или https://api.site.com
   withCredentials: true,
